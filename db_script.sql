5b01a7b4739178207d6409d664e7b2edcc08332f
mayurkarale34 <mayurkarale34@gmail.com>
2024-02-11 16:50:23 +0530

    login page connectivity

---
+++ db_script.sql
@@ -0,0 +1,9 @@
+-- Create new database
+create database employee_new;
+
+-- Select created database
+use employee_new;
+
+
+
+
--- modules/index.py
+++ modules/index.py
@@ -2,3 +2,7 @@
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
