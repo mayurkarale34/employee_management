b2d8974ba62fa9435b80d635aba896fbc242a6cf
mayurkarale34 <mayurkarale34@gmail.com>
2024-02-21 10:42:24 +0530

    login changes

--- modules/index.py
+++ modules/index.py
@@ -10,3 +10,14 @@ def login():
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+
+@app.route('/validate_login', methods = ['POST'])
+def validate_login():
+    data = dict(request.form)
+    user_name = data['username']
+    password = data['password']
+    if user_name.upper() == 'ADMIN' and password.upper()=='ADMIN':
+        return jsonify({"status" : True})
+    else:
+        return jsonify({"status" : False})
