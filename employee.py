5c286c81ac301648b963a281ab5c45e167cd3935
sujata027 <sujata027@gmail.com>
2024-03-02 20:19:07 +0530

    manage metadata page navbar

--- employee.py
+++ employee.py
@@ -9,4 +9,32 @@ def init_engine(app):
 app = Flask(__name__)
 app.debug = True
 init_engine(app)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/home')
+def home():
+    user_name = "Admin"
+    return render_template('home.html', username = user_name)
+
+
+@app.route('/validate_login', methods = ['POST'])
+def validate_login():
+    data = dict(request.form)
+    user_name = data['username']
+    password = data['password']
+    if user_name.upper() == 'ADMIN' and password.upper()=='ADMIN':
+        return jsonify({"status" : True})
+    else:
+        return jsonify({"status" : False})
+    
+@app.route('/empmanagement')
+def empmanagement():
+    user_name = "Admin"
+    return render_template('empmanagement.html', username = user_name)
 app.run(port = 5000)
