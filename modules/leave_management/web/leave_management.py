f39b03398f6660c1a54e907786a33040c960d297
sujata027 <sujata027@gmail.com>
2024-03-26 12:33:34 +0530

    quick fixes

--- modules/leave_management/web/leave_management.py
+++ modules/leave_management/web/leave_management.py
@@ -7,3 +7,30 @@ def leave_management():
     except Exception as e:
         print("Exception in leave_management() : ", str(e))
         return redirect('/home')
+    
+     
+    # add leave
+@app.route('/add_leave_management', methods=['POST'])
+def add_leave_managenent():
+    connection = app._engine.connect()
+    transaction = connection.begin()
+    try:
+        request_data = dict(request.form)
+        data = {
+            "leave_type" : request_data['leave_type'],
+            "start_date" : request_data['start_date'],
+            "end_date" : request_data['end_date'],
+            "duration" : request_data['duration'],
+            "leave_status" : request_data['leave_status'],
+           
+        }
+        
+        add_metadata_response = add_to_database(data, "tb_leave", connection)
+        transaction.commit()
+        connection.close()
+        return redirect('/leave_management')
+    except Exception as e:
+        transaction.rollback()
+        connection.close()
+        print(str(e))
+        return redirect('/leave_management')
