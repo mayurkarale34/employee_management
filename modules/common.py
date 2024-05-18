c5fdd6a7cafe1933ea3cbfd3a8eba5b2a04649d7
mayurkarale34 <149086041+mayurkarale34@users.noreply.github.com>
2024-05-18 15:54:17 +0530

    Update common.py

--- modules/common.py
+++ modules/common.py
@@ -1,3 +1,28 @@
+# common add to database
+def add_to_database(data, table, connection):
+    response = {
+        "status" : False,
+        "message" : ""
+    }
+    try:
+        # Constructing the INSERT query dynamically
+        columns = ', '.join(data.keys())
+        values = ""
+        for val in data.values():
+            if len(values):
+                values = values + ', '+ "'" + val + "'"
+            else:
+                values = "'" + val + "'"
+        insert_query = text(f"INSERT INTO {table} ({columns}) VALUES ({values});")
+
+        # Executing the query with the provided connection
+        connection.execute(insert_query)
+        
+        response['status'] = True
+        return response
+    except Exception as e:
+        print(str(e))
+        return response
 
 def check_duplicate_metadata(data):
     response = {
